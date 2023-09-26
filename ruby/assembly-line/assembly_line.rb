c16825e1b8f09a8939e60f0223c47e0000b64626
Marcos Urbanski <marcos.urbanski@gmail.com>
2023-09-26 15:47:40 -0300

    current version

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -4,10 +4,41 @@ class AssemblyLine
   end
 
   def production_rate_per_hour
+    if @speed < 5
+      return @speed * 221            
+    elsif @speed >= 5 || @speed <= 8
+      @speed = @speed * 221
+      @speed = @speed *0.90
+      return @speed
+    elsif @speed > 8 && @speed <10
+      @speed = @speed * 221
+      @speed = @speed * 0.80
+      return  @speed
+    else
+      @speed = @speed * 221
+      @speed = @speed * 0.70
+      return @speed
+    end
     raise 'Please implement the AssemblyLine#production_rate_per_hour method'
   end
 
   def working_items_per_minute
+    if @speed == 1
+      return production_rate_per_hour(@speed)/60.to_i            
+    elsif @speed == 5 || @speed == 8 || @speed == 9
+      @speed = @speed * 221
+      @speed = @speed *0.90
+      return production_rate_per_hour(@speed)/60.to_i  
+    elsif @speed == 9
+      @speed = @speed * 221
+      @speed = @speed * 0.80
+      return production_rate_per_hour(@speed)/60.to_i  
+    else
+      @speed = @speed * 221
+      @speed = @speed * 0.70
+      return production_rate_per_hour(@speed)/60.to_i  
+    end
+
     raise 'Please implement the AssemblyLine#working_items_per_minute method'
   end
 end
