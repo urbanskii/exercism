0dec6b328b456f457506aed8a5eb176d1e05ab76
urbanskii <marcos.urbanski@gmail.com>
2023-09-26 16:54:57 -0300

    first try

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -5,3 +5,15 @@ Write your code for the 'Tournament' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/tournament` directory.
 =end
+
+
+class Tournament
+    def initialize(input)
+        @input = input
+    end
+
+    def tally
+        table = {team:{"Devastating Donkeys"; "Allegoric Alaskans"; "Blithering Badgers"; "Courageous Californians"}, MP:{}, W:{}, D:{}, L:{}, P:{}}
+    end
+
+end
